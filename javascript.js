e1a472123c0e5935e6b2728145f757bb3ee3339f
Taiki Maekawa <maekawataiki@yahoo.co.jp>
2017-11-05 11:27:35 -0500

    Updated user branch from Portal workspace (javascript.js)

--- javascript.js
+++ javascript.js
@@ -41,37 +41,20 @@ angular.module('portalApp')
 
 		// Place your init code here:
 		data.value={message:"Welcome to Portal SDK!"};
+        // sample data
 		items.value = [
 			{
-				title:'Item 1',
-				tags: ['tag A', 'tag B', 'tag C'],
-				details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+                id:'gary-danko-san-francisco',
+                name: 'Gary Danko',
+                url: 'https://www.yelp.com/biz/gary-danko-san-francisco',
+                price: '$$$$',
+                rating: 4.5,
+                review_count: 4521,
+                today_start: '1730',
+                today_end: '2200',
+                tomorrow_start: '1730',
+                today_end: '2200'
 			},
-			{
-				title:'Item 2',
-				tags: ['tag D', 'tag E', 'tag F'],
-				details: 'Mauris cursus, sapien et malesuada ultrices, purus sapien iaculis tellus, quis semper magna est at leo.'
-			},
-			{
-				title:'Item 3',
-				tags: ['tag A', 'tag H'],
-				details: 'Donec id quam eu odio feugiat sagittis. Duis a tempus neque. Praesent elementum quis ante quis commodo. Sed tincidunt aliquet dolor sit amet laoreet. '
-			},
-			{
-				title:'Item 4',
-				tags: ['tag I'],
-				details: 'Proin sem quam, rutrum id ante id, scelerisque tempor quam. Curabitur pharetra turpis at sem placerat, non vehicula ligula tincidunt.'
-			},
-			{
-				title:'Item 5',
-				tags: ['tag C', 'tag K', 'tag B'],
-				details: 'Mauris nec ultricies metus. Cras et dictum justo. Nam a ullamcorper dolor. Cras fringilla metus vel facilisis vehicula.'
-			},
-			{
-				title:'Item 6',
-				tags: ['tag A', 'tag B', 'tag C'],
-				details: 'Curabitur scelerisque lorem risus, in luctus orci hendrerit non. Praesent quis tellus dapibus dolor consectetur volutpat.'
-			}
 		];
 	}
 
